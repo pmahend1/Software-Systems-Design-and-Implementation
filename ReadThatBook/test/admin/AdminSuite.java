dafe0dc89e9c4f4925b52c97a9b88f3b28dddf35
ashKadam <akadam3@uncc.edu>
2016-10-30 18:53:02 -0400

    Add files via upload

---
+++ ReadThatBook/test/admin/AdminSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author akshay
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({admin.AuthenticationServletTest.class, admin.BookManagerTest.class, admin.UserProfileManagerTest.class, admin.BookListServletTest.class, admin.HomeServletTest.class})
+public class AdminSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
---
+++ ReadThatBook/test/admin/HomeServletTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author akshay
+ */
+public class HomeServletTest {
+    
+    public HomeServletTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of doGet method, of class HomeServlet.
+     */
+    @Test
+    public void testDoGet() throws Exception {
+        System.out.println("doGet");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        HomeServlet instance = new HomeServlet();
+        instance.doGet(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doPost method, of class HomeServlet.
+     */
+    @Test
+    public void testDoPost() throws Exception {
+        System.out.println("doPost");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        HomeServlet instance = new HomeServlet();
+        instance.doPost(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
