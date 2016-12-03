c67295d3f2b13ecbef41f489e23b41843ff98a28
pmahend1 <pmahend1@uncc.edu>
2016-12-03 17:33:32 -0500

    BookDBTest updated test cases

--- ReadThatBook/test/data/BookDBTest.java
+++ ReadThatBook/test/data/BookDBTest.java
@@ -6,7 +6,9 @@
 package data;
 
 import business.Book;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
@@ -21,51 +23,27 @@ import static org.junit.Assert.*;
  * @author akshay
  */
 public class BookDBTest {
-    
+
     public BookDBTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of addBookImage method, of class BookDB.
-     */
-    @Test
-    public void testAddBookImage() {
-        System.out.println("addBookImage");
-        int bookID = 0;
-        InputStream inputStream = null;
-        int expResult = 0;
-        int result = BookDB.addBookImage(bookID, inputStream);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of updateBookImage method, of class BookDB.
-     */
-    @Test
-    public void testUpdateBookImage() {
-        System.out.println("updateBookImage");
-        int bookID = 0;
-        InputStream inputStream = null;
-        int expResult = 0;
-        int result = BookDB.updateBookImage(bookID, inputStream);
-        assertEquals(expResult, result);
-    }
+    
 
     /**
      * Test of addBook method, of class BookDB.
@@ -73,10 +51,15 @@ public class BookDBTest {
     @Test
     public void testAddBook() {
         System.out.println("addBook");
-        Book book = null;
-        int expResult = 0;
+        Book book;
+        book = new Book("E", "A", "4564", "645", "Fantasy", "1", "McGraw", "Book A");
         int result = BookDB.addBook(book);
-        assertEquals(expResult, result);
+        String title = "E";
+        int bookidDB = BookDB.getBookIDByISBN("4564");
+        Book bookDb = BookDB.selectBook(bookidDB);
+        String titleDB = bookDb.getTitle();
+        System.out.println(titleDB);
+        assertEquals(title, titleDB);
     }
 
     /**
@@ -85,12 +68,46 @@ public class BookDBTest {
     @Test
     public void testUpdateBook() {
         System.out.println("updateBook");
-        Book book = null;
-        int expResult = 0;
+        Book book = BookDB.selectBook(BookDB.getBookIDByISBN("4564"));
+        book.setTitle("new Title");
         int result = BookDB.updateBook(book);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        Book updatedBook = BookDB.selectBook(BookDB.getBookIDByISBN("4564"));
+        String newTitle = updatedBook.getTitle();
+        assertEquals(newTitle, "new Title");
+    }
+    
+    /**
+     * Test of addBookImage method, of class BookDB.
+     */
+    @Test
+    public void testAddBookImage() throws UnsupportedEncodingException {
+        System.out.println("addBookImage");
+        int bookID = BookDB.getBookIDByISBN("4564");
+        String imageUrl = "http://tse1.mm.bing.net/th?id=OIP.M7c14cc7de0243cc221aaca16f06a9eb5o2&w=246&h=183&c=7&rs=1&qlt=90&o=4&pid=1.1";
+        InputStream inputStream = new ByteArrayInputStream(imageUrl.getBytes("UTF-8"));
+        
+        int result = BookDB.addBookImage(bookID, inputStream);
+        byte[] dbValue = BookDB.getBookImage(bookID);
+        String dbImageUrl = new String(dbValue,"UTF-8");
+        System.out.println(dbImageUrl);
+        assertEquals(imageUrl, dbImageUrl);
+    }
+
+    /**
+     * Test of updateBookImage method, of class BookDB.
+     */
+    @Test
+    public void testUpdateBookImage() throws UnsupportedEncodingException {
+        System.out.println("updateBookImage");
+        int bookID = 4;
+        String testValue = "abc";
+        InputStream is = new ByteArrayInputStream(testValue.getBytes("UTF-8"));
+        BookDB.updateBookImage(bookID, is);
+        byte[] dbValues = BookDB.getBookImage(bookID);
+        String resultStringDB = new String(dbValues, "UTF-8");
+        System.out.println(dbValues);
+        System.out.println(resultStringDB);
+        assertEquals(testValue, resultStringDB);
     }
 
     /**
@@ -99,10 +116,12 @@ public class BookDBTest {
     @Test
     public void testSelectBook() {
         System.out.println("selectBook");
-        int bookID = 0;
-        Book expResult = null;
+        int bookID = 1;
+
         Book result = BookDB.selectBook(bookID);
-        assertEquals(expResult, result);
+        System.out.println(result.getTitle());
+
+        assertEquals("Pride & Predujice", result.getTitle());
     }
 
     /**
@@ -111,10 +130,10 @@ public class BookDBTest {
     @Test
     public void testDeleteBook() {
         System.out.println("deleteBook");
-        int bookID = 0;
-        int expResult = 0;
+        int bookID = BookDB.getBookIDByISBN("4564");
         int result = BookDB.deleteBook(bookID);
-        assertEquals(expResult, result);
+        Book retriveBook = BookDB.selectBook(bookID);
+        assertEquals(retriveBook, null);
     }
 
     /**
@@ -123,21 +142,31 @@ public class BookDBTest {
     @Test
     public void testSelectAllBooks() {
         System.out.println("selectAllBooks");
-        List<Book> expResult = null;
         List<Book> result = BookDB.selectAllBooks();
-        assertEquals(expResult, result);
+       
+        //valid test case
+        int sum=0;
+        if(result!=null){
+            for(Book book : result){
+                System.out.println(book.getBookID());
+                sum=sum+book.getBookID();
+            }
+        }
+        assertTrue(sum>0);
     }
 
     /**
      * Test of getBookImage method, of class BookDB.
      */
     @Test
-    public void testGetBookImage() {
+    public void testGetBookImage() throws UnsupportedEncodingException {
         System.out.println("getBookImage");
-        int bookID = 0;
-        byte[] expResult = null;
-        byte[] result = BookDB.getBookImage(bookID);
-        assertArrayEquals(expResult, result);
+        int bookID = 4;
+        
+        String expResult = "abc";
+        byte[] dbBytes = BookDB.getBookImage(bookID);
+        String dbResult  = new String(dbBytes, "UTF-8");
+        assertEquals(expResult, dbResult);
     }
 
     /**
@@ -146,10 +175,11 @@ public class BookDBTest {
     @Test
     public void testSearchBook() {
         System.out.println("searchBook");
-        String bookTitle = "";
-        Book expResult = null;
+        String bookTitle = "Pride & Predujice";
         List<Book> result = BookDB.searchBook(bookTitle);
-        assertEquals(expResult, result);
+        String dbBookTitle = BookDB.searchBook(bookTitle).get(0).getTitle();
+        System.out.println(dbBookTitle);
+        assertEquals(bookTitle, dbBookTitle);
     }
 
     /**
@@ -158,8 +188,8 @@ public class BookDBTest {
     @Test
     public void testCheckBookImageExists() {
         System.out.println("checkBookImageExists");
-        int bookID = 0;
-        boolean expResult = false;
+        int bookID = 1;
+        boolean expResult = true;
         boolean result = BookDB.checkBookImageExists(bookID);
         assertEquals(expResult, result);
 
@@ -171,11 +201,11 @@ public class BookDBTest {
     @Test
     public void testGetBookIDByISBN() {
         System.out.println("getBookIDByISBN");
-        String ISBN = "";
-        int expResult = 0;
+        String ISBN = "1234567890";
+        int expBookId = 1;
         int result = BookDB.getBookIDByISBN(ISBN);
-        assertEquals(expResult, result);
+        assertEquals(expBookId, result);
 
     }
-    
+
 }
