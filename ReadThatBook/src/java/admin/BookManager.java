c23fee01a38670fe7ff300b9431b8a4ce7b488ea
ashKadam <akadam3@uncc.edu>
2016-12-03 18:50:02 -0500

    Update BookManager.java

--- ReadThatBook/src/java/admin/BookManager.java
+++ ReadThatBook/src/java/admin/BookManager.java
@@ -307,6 +307,39 @@ public class BookManager extends HttpServlet {
             request.setAttribute("bookResult", bookList);
             getServletContext().getRequestDispatcher(url).forward(request, response);
             }
-        }       
+        }else if (action.equals("advancedSearchBook")) {
+            url="/advancedSearch.jsp";
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        }else if (action.equals("advSearchBook")) {
+            String bookTitle = request.getParameter("searchBookTitle");
+            String bookAuthor = request.getParameter("searchBookAuthor");
+            String bookGenre = request.getParameter("searchBookGenre");
+            String bookISBN = request.getParameter("searchBookISBN");
+            List<Book> bookList = BookDB.advSearchBook(bookTitle, bookAuthor, bookGenre, bookISBN);
+            if(bookList.isEmpty()){
+                System.out.println("admin.BookManager.doPost()" + "Book Does not exist");
+                cookies = request.getCookies();
+                
+                for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("userCookie")){
+                    url = "/home.jsp";
+                    User user = UserDB.selectUser(cookie.getValue());
+                    request.setAttribute("user", user);
+                }
+                else 
+                    url = "/guestHome.jsp";
+                }
+                
+                message= "Book does not exist with this title or author.";
+                request.setAttribute("searchErrorMessage", message);
+                List<Book> books = BookDB.selectAllBooks();   
+                request.setAttribute("books", books);
+                request.getServletContext().getRequestDispatcher(url).forward(request, response);
+            }else{
+                url="/viewSearchResult.jsp";
+                request.setAttribute("bookResult", bookList);
+                getServletContext().getRequestDispatcher(url).forward(request, response);
+            }
+        }
     }
 }
