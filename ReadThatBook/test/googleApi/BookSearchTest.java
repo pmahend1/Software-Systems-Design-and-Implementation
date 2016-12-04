1f18f1ba2211c038240180d0c5ad0fc0046af1bd
pmahend1 <pmahend1@uncc.edu>
2016-12-03 22:13:10 -0500

    BookSearch bug fix

--- ReadThatBook/test/googleApi/BookSearchTest.java
+++ ReadThatBook/test/googleApi/BookSearchTest.java
@@ -69,6 +69,7 @@ public class BookSearchTest {
         String args = "Harry";
         String expResult = "Harry";
         String result = BookSearch.parseQuery(args);
+        System.out.println(result);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
