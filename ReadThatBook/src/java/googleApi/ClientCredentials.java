d8c97f2dbe656944b4b35cd285c7c75c2b638e0e
pmahend1 <pmahend1@uncc.edu>
2016-12-19 20:31:06 -0500

    Update ClientCredentials.java

--- ReadThatBook/src/java/googleApi/ClientCredentials.java
+++ ReadThatBook/src/java/googleApi/ClientCredentials.java
@@ -4,7 +4,7 @@ package googleApi;
 public class ClientCredentials {
 
   /** Value of the "API key" shown under "Simple API Access". */
-  static final String API_KEY = "AIzaSyAQtL-T66XXXVYhbSDXO5v-Q6mJjd5VnhU";
+  static final String API_KEY = "************************";
      // + ClientCredentials.class;
 //"Enter API Key from https://code.google.com/apis/console/?api=books into API_KEY in "
   static void errorIfNotSpecified() {
